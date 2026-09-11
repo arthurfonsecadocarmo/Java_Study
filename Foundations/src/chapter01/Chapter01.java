24d847c9cb83433b2f5a3dbae17ac1104acfd555
arthurfonsecadocarmo <arthur.fonsecarmo@gmail.com>
2026-09-11 16:03:14 -0300

    complete Java introduction

---
+++ Foundations/src/chapter01/Chapter01.java
@@ -0,0 +1,10 @@
+package chapter01;
+
+public class Chapter01 {
+    static void main(String[] args) {
+        System.out.println("Java Source Code: .java");
+        System.out.println("Compilation with javac");
+        System.out.println("Bytecode: .class");
+        System.out.println("Execution with JVM");
+    }
+}
