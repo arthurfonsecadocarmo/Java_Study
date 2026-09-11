a3420ddab7f33ea7b56785874a8fe6fdc32d001a
arthurfonsecadocarmo <arthur.fonsecarmo@gmail.com>
2026-09-11 15:45:33 -0300

    Add Java introduction examples

--- Foundations/src/Main.java
+++ Foundations/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] Args) {
-        System.out.println("Started Journey");
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+        System.out.println("I'm learning how java works.");
+        System.out.println("This program is running on JVM.");
     }
 }
